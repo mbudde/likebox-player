c081e8910ee30c2a849e4055eae941ef22e2e46e
Michael Budde <mbudde@gmail.com>
2011-04-17 20:57:59 +0200

    Update setup.py

--- setup.py
+++ setup.py
@@ -3,10 +3,18 @@
 from distutils.core import setup
 
 setup(
-    name='Likebox',
-    version='0.1',
-    description='Musicplayer with voting',
-    author='Likebox developers',
-    packages=['likebox'],
-    scripts=['bin/likebox'],
+    name = 'Likebox',
+    version = '0.1',
+    description = 'Musicplayer with voting',
+    author = 'Likebox developers',
+    packages = [
+        'likebox'
+    ],
+    scripts = [
+        'bin/likebox',
+    ],
+    requires = [
+        'PyQT',
+        'pyhon-mpd',
+    ],
 )
