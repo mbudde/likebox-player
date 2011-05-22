1f5c04cec29ae95b83ef92d6af2bfbce4702fd15
Michael Budde <mbudde@gmail.com>
2011-05-22 14:00:46 +0200

    Use divmod

--- likebox/utils.py
+++ likebox/utils.py
@@ -4,8 +4,8 @@ from __future__ import division
 def format_time(seconds):
     if isinstance(seconds, basestring):
         seconds = int(seconds)
-    hours, rest = seconds // (60*60), seconds % (60*60)
-    minutes, seconds = rest // 60, rest % 60
+    hours, rest = divmod(seconds, 60*60)
+    minutes, seconds = divmod(rest, 60)
     if hours > 0:
         return '{0}:{1:02}:{2:02}'.format(hours, minutes, seconds)
     else:
